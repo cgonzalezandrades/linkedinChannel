ec1f01128d63d852210e9cd5ec33448c852161a0
Carlos Gonzalez <cubeton@gmail.com>
2017-08-06 14:36:07 -0500

    mysql connection

--- public/js/HomeController.js
+++ public/js/HomeController.js
@@ -1,43 +1,69 @@
-myApp.controller('HomeController', ['$scope',  '$rootScope', '$state', '$stateParams', '$http',
+myApp.controller('HomeController', ['$scope', '$rootScope', '$state', '$stateParams', '$http',
     function ($rootScope, $scope, $state, $stateParams, $http) {
 
-      console.log('ins home controller')
-      $scope.newUser={};
-      $scope.userIn = true;
-      
+    console.log('ins home controller')
+    $scope.newUser = {};
+    $scope.userIn = true;
+
+
+    $scope.getAccounts = function () {
       $http.get('/accountList').then(function success(response) {
         console.log(response.data)
-                console.log("I got the data I requested");
-                // console.log(response);
-          $scope.users = response.data;
-//        $scope.users = {something:'ss', another:'sds'}
-      });
-      
-//  $scope.users = {something:'ss', another:'sds'}
-      
-      $scope.formFilled = function(){
-        
-        console.log($scope.newUser)
-        
-        if($scope.newUser.firstName.length > 0 && $scope.newUser.lastName.length > 0 && $scope.newUser.link.length > 0){
-            $scope.userIn = false;
-        console.log('sd')
-        }else{
-          $scope.userIn = true;
+        console.log("I got the data I requested");
+        $scope.users = response.data;
+
+        for (var i = 0; i < $scope.users.length; i++) {
+
+          var dateSplit = response.data[i].DATE_CREATED.split(/[- :]/);
+          var dayStrip = dateSplit[2].split('T');
+          var date = dateSplit[0] + '/' + dateSplit[1] + '/' + dayStrip[0];
+          console.log(date)
+
+          $scope.users[i].DATE_CREATED = date;
+          $scope.users[i].ADDED = false;
+          
         }
-        
+
+      });
+    }
+    $scope.getAccounts();
+    //  $scope.users = {something:'ss', another:'sds'}
+
+    $scope.userConnected = function(index){
+      console.log(index)
+      $scope.users[index].ADDED = true;
+    }  
       
+    $scope.formFilled = function () {
+
+      console.log($scope.newUser)
+
+      if ($scope.newUser.ACCOUNT_FIRST_NAME.length > 0 && $scope.newUser.ACCOUNT_LAST_NAME.length > 0 && $scope.newUser.ACCOUNT_LINK.length > 0) {
+        $scope.userIn = false;
+        console.log('sd')
+      } else {
+        $scope.userIn = true;
       }
-      
-      $scope.addNewUser = function(){
-        
-        console.log($scope.newUser)
-        
-         $scope.users.push({
-           ACCOUNT_FIRST_NAME:$scope.newUser.firstName,
-           ACCOUNT_LAST_NAME:$scope.newUser.lastName,
-           ACCOUNT_LINK:$scope.newUser.link
-         })
-      }
+
+
+    }
+
+    $scope.addNewUser = function () {
+
+
+
+
+
+      $http.post('/addAccount', $scope.newUser).then(function success(response) {
+
+      }, function errorCallback(error) {
+        console.log(error)
+      });
+
+
+      $scope.getAccounts();
+      $scope.newUser = {};
+
+    }
 
 }]);
--- server.js
+++ server.js
@@ -3,13 +3,15 @@ var app = express();
 var bodyParser = require('body-parser');
 var connection = require('./public/config/connection')
 
-console.log(connection)
+//console.log(connection)
 
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 connection.connect(function (err) {
   if (err) {
@@ -26,61 +28,49 @@ connection.connect(function (err) {
 
 
 app.get('/', function (req, res) {
-    
-      res.sendFile('index.html');
+
+  res.sendFile('index.html');
 })
 
-app.get('/accountList',function (req, res) {
+app.get('/accountList', function (req, res) {
 
-    console.log("I received a get request.");
+  //    console.log("I received a get request.");
 
-    //Dummie Data
-    // person1 = {
-    //     name:"Tim",
-    //     email:"tim@email.com",
-    //     number :"111-111-1111"
-    // };
-    //
-    // person2={
-    //     name:'emely',
-    //     email:'emely@email.com',
-    //     number:'222-222-2222'
-    // };
-    //
-    // person3={
-    //     name:'John',
-    //     email:'John@email.com',
-    //     number:'333-333-3333'
-    // };
 
-//    db.contactlist.find(function (err,docs) {
-//
-//        console.log(docs);
-//
-//        res.json(docs);
-//    });
-  
   connection.query('SELECT * FROM LINKEDIN_ACCOUNTS', function (error, results, fields) {
-  if (error) throw error;
-  // connected! 
-  
-  var formattedData = JSON.stringify(results)
-   
-//  console.log(results[0].ACCOUNT_FIRST_NAME)
-  console.log(results)
-   res.json(results);
+    if (error) throw error;
+    // connected! 
+
+    var formattedData = JSON.stringify(results)
+
+    //  console.log(results[0].ACCOUNT_FIRST_NAME)
+    //  console.log(results)
+    res.json(results);
+  });
+
+  // var contactList = [person1,person2,person3];
+  //
+  // res.json(contactList);
+
 });
 
-    // var contactList = [person1,person2,person3];
-    //
-    // res.json(contactList);
 
-});
+app.post('/addAccount', function (req, res) {
+  console.log('this is the body')
+  console.log(req.body)
 
+  connection.query('INSERT INTO LINKEDIN_ACCOUNTS (ACCOUNT_FIRST_NAME, ACCOUNT_LAST_NAME, ACCOUNT_LINK) VALUES(?,?,?)', [req.body.ACCOUNT_FIRST_NAME, req.body.ACCOUNT_LAST_NAME, req.body.ACCOUNT_LINK], function (error, results, field) {
+    if (error) throw error;
+
+
+  })
+
+
+})
 
 
 var PORT = process.env.PORT || 3000;
 app.listen(PORT, function () {
-    console.log('IM LISTENING IS PORT ' + PORT);
+  console.log('IM LISTENING IS PORT ' + PORT);
 
 })
