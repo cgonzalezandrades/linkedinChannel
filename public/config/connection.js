cf2f1b15d8a0fa2aa3c1987d601170dfbb59f8ee
Carlos Gonzalez <cubeton@gmail.com>
2017-08-06 14:53:29 -0500

    mysql connection

--- public/config/connection.js
+++ public/config/connection.js
@@ -5,10 +5,17 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
 
+  //  connection = mysql.createConnection({
+  //    host: 'localhost',
+  //    user: '',
+  //    password: '',
+  //    database: 'LINKEDIN'
+  //  });
+
   connection = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: '',
+    user: 'kh25u3iu1dz254qk',
+    password: 'a26s5k6hjtq66m4p',
     database: 'LINKEDIN'
   });
 
